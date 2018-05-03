4b35e683661fb2ccae4275227dedc1c276aa54da
wilder <champion.bas@gmail.com>
2018-05-04 00:00:58 +0200

    title partenaires

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -11,31 +11,65 @@ import { element } from 'protractor';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) private doc: Document) { }
+  emailArray = [];
+  changedChallenge: boolean = false;
 
-  ngOnInit() {
-    var countDownDate = new Date("June 23, 2018 15:37:25").getTime();
-    var x = setInterval(function () {
-      var now = new Date().getTime();
-      var distance = countDownDate - now;
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      document.getElementById("demo").innerHTML = days + "J " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-      }
-    }, 1000);
+  addTaskValue = '';
 
-    window.addEventListener("scroll", function (event) {
-      var scroll = this.scrollY;
-      if (scroll == 0) {
-        document.getElementById('changecolor').style.backgroundColor = "transparent";
-      } else {
-        document.getElementById('changecolor').style.backgroundColor = "black";
-      }
-    });
-  }}
+  onClickSubmit(data) {
+    if (data.emailid >= 0) {
+      alert('veuillez entrer une adresse mail !');
+    } else {
+      this.emailArray.push(this.addTaskValue);
+      this.addTaskValue = '';
+      // data.emailid = '';
+      console.log(this.emailArray);
+    }
+  }
+
+  nextchallengefn() {
+    document.getElementById('challenge1').style.opacity = "0";
+    document.getElementById('challenge2').style.left = "0";
+    document.getElementById('challenge2').style.opacity = "1";
+    this.changedChallenge = true;
+  }
+
+  previouschallengefn() {
+    document.getElementById('challenge1').style.opacity = "1";
+    document.getElementById('challenge2').style.opacity = "0";
+    document.getElementById('challenge2').style.left = "100vw";
+    
+    this.changedChallenge = false;
+  }
+
+constructor(@Inject(DOCUMENT) private doc: Document) { }
+
+ngOnInit() {
+  var countDownDate = new Date("June 23, 2018 13:00:00").getTime();
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = days + "J " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+  window.addEventListener("scroll", function (event) {
+    var scroll = this.scrollY;
+    if (scroll == 0) {
+      document.getElementById('changecolor').style.backgroundColor = "transparent";
+    } else {
+      document.getElementById('changecolor').style.backgroundColor = "black";
+    }
+  });
+}
+}
+
+
